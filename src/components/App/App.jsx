1fd08484729c699faeec8cc45721839e1bea3ad3
Anant Kaushik <anant.kaushik@asteyax.com>
2019-04-21 13:10:44 +0530

    added building assets

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import "assets/fonts/flaticon/flaticon.css";
+import "assets/fonts/font-awesome/font-awesome.min.css";
 class App extends Component {
   render() {
     return (
