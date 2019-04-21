4febae04913eed18b451530923472d63fc8a1477
Anant Kaushik <anant.kaushik@asteyax.com>
2019-04-21 13:11:05 +0530

    added routing

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,13 +1,22 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import routes from "routes";
+import "./App.css";
 
 import "assets/fonts/flaticon/flaticon.css";
 import "assets/fonts/font-awesome/font-awesome.min.css";
 class App extends Component {
+  getRoutes = routes => {
+    return routes.map((prop, key) => {
+      return <Route path={prop.path} component={prop.component} key={key} />;
+    });
+  };
   render() {
     return (
-      <div className="App">
-      </div>
+      <Router>
+        <div className="App" />
+        <Switch>{this.getRoutes(routes)}</Switch>
+      </Router>
     );
   }
 }
---
+++ src/routes/index.jsx
@@ -0,0 +1,10 @@
+import HomePage from "views/HomePage.jsx";
+
+const routes = [
+  {
+    path: "/",
+    component: HomePage
+  }
+];
+
+export default routes;
