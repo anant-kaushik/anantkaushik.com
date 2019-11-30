01feb994434cec4be0e2bb01c871ccfc0a53ce3e
Anant Kaushik <anantkaushik@Anants-MBP.wv.cc.cmu.edu>
2019-11-29 20:55:22 -0500

    added link to blank target and new resume

--- src/components/LinkRefer/LinkRefer.jsx
+++ src/components/LinkRefer/LinkRefer.jsx
@@ -3,7 +3,7 @@ import React from "react"
 import "./LinkRefer.css";
 
 const LinkRefer = ({ children, link }) => (
-    <a className="link-refer" href={link}>{children}</a>
+    <a className="link-refer" href={link} target="_blank">{children}</a>
 );
 
 export default LinkRefer;
