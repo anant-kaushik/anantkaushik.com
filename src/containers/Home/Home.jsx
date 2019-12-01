94af364a472b2cb4b6fa486ed9e1dc5ef3cb6b74
Anant Kaushik <anantkaushik@Anants-MBP.wv.cc.cmu.edu>
2019-11-30 20:34:31 -0500

    updated text

--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -12,24 +12,23 @@ import { Row } from "react-bootstrap";
 
 const AboutMe = ({ about }) => (
     <Section>
-        <SectionTitle title="About Me" />
+        <SectionTitle title="Hi! I am Anant" />
         <Paragragh className="top_30">
             I'm currently pursuing my  <LinkRefer link="https://mse.isri.cmu.edu/applicants/mse-ap/index.html">Master of Software Engineering at Carnegie Mellon University</LinkRefer>,
             and I'm looking for software development internships in challenging projects for summer 2020.
             <br />
             <br />
-            Before coming to CMU, I was the Lead developer of Zerone Microsystems Private Limited, one of the four Indian startups selected for the Anthill Ventures’ A- Scale program.
-            The former KPMG CEO and former KPMG partner founded this firm. I worked on the core technology and led the development of several use - cases like an
+            Before joining CMU, I was the Lead developer at Zerone Microsystems Private Limited, one of the four Indian startups selected for the Anthill Ventures’ A- Scale program which was founded by the team of a former KPMG CEO and Partner.
+            I worked on the core technology and led the development of several use - cases which included an
             <LinkRefer link="http://conyct.com"> identity exchange app</LinkRefer>, and
             <LinkRefer link="http://conyct.com/vms"> visitor management web and mobile application</LinkRefer>,
             <LinkRefer link="http://zpi.cash"> payment exchange app</LinkRefer>.
             <br />
             <br />
-            In my master's, I have been learning the fundamentals of software engineering complemented and what it truly means to create a scalable system from scratch.
+            Through my Master's, I am trying to learn the fundamentals of software engineering and what it truly means to create a scalable system from scratch.
             <br />
             <br />
-            Apart from that, startups and innovations are very dear to me. My elective Entrepreneurship and Innovation in Technology allowed me to connect with cutting-edge startup CEOs, VCs, and angles and learn about the other side of the coin, straight from the horse's mouth.
-
+            Apart from this, I have a love for startups and creative innovations. My elective Entrepreneurship and Innovation in Technology allowed me to connect with cutting-edge startup CEOs, VCs, and Angels which has helped me learn about the other side of the coin straight from the horse's mouth.
         </Paragragh>
     </Section>
 )
