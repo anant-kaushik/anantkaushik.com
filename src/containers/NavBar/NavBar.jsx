190475c5f76c0f76f69e62253af4622ba912514c
Anant Kaushik <anantkaushik@Anants-MacBook-Pro.local>
2020-04-05 23:59:06 -0400

    changed resume and added links

--- src/containers/NavBar/NavBar.jsx
+++ src/containers/NavBar/NavBar.jsx
@@ -9,61 +9,75 @@ import { data } from "utils";
 const EMAIL = data.email;
 
 const TabButton = ({ history, route }) => (
-    <li className={"tab" + (history.location.pathname === route.path ? " active" : "")}>
-        <button
-            className={route.home ? "home-btn" : ""}
-            onClick={() => {
-                history.push(route.path);
-            }}>
-            {route.home ?
-                <i className="fa fa-home" aria-hidden="true" /> :
-                route.name
-            }
-        </button>
-    </li>
+  <li
+    className={
+      "tab" + (history.location.pathname === route.path ? " active" : "")
+    }
+  >
+    <button
+      className={route.home ? "home-btn" : ""}
+      onClick={() => {
+        history.push(route.path);
+      }}
+    >
+      {route.home ? (
+        <i className="fa fa-home" aria-hidden="true" />
+      ) : (
+        route.name
+      )}
+    </button>
+  </li>
 );
 
 const Tabs = ({ history, routes }) => (
-    <ul className="tabs">
-        {routes.map(route =>
-            <TabButton
-                history={history}
-                route={route} />
-        )}
-    </ul >
-)
+  <ul className="tabs">
+    {routes.map((route) => (
+      <TabButton history={history} route={route} />
+    ))}
+  </ul>
+);
 
-const Links = () => (
-    <div>
-        <a href={"mailto:" + EMAIL} className="pull-right site-btn icon hidden-xs">Mail<i className="fa fa-paper-plane" aria-hidden="true"></i></a>
-    </div>
+const SocialIcons = ({ github, linkedin }) => (
+  <div className="hidden-md social-icons pull-right">
+    <a className="gh" href={github.href} target="_blank">
+      <i className="fa fa-github" aria-hidden="true"></i>
+    </a>
+    <a className="ld" href={linkedin.href} target="_blank">
+      <i className="fa fa-linkedin" aria-hidden="true"></i>
+    </a>
+  </div>
+);
 
-)
+const Links = ({ links }) => (
+  <div>
+    <a href={"mailto:" + EMAIL} className="pull-right site-btn icon hidden-xs">
+      Mail<i className="fa fa-paper-plane" aria-hidden="true"></i>
+    </a>
+    <SocialIcons {...links.socials} />
+  </div>
+);
+
 const Nav = ({ children }) => (
-    <header>
-        <nav>
-            <Row>
-                {children}
-            </Row>
-        </nav>
-    </header>
-)
+  <header>
+    <nav>
+      <Row>{children}</Row>
+    </nav>
+  </header>
+);
 
 class NavBar extends Component {
-    render() {
-        return (
-            <Nav>
-                <Col md="8" sm="8" xs="4">
-                    <Tabs
-                        history={this.props.history}
-                        routes={routes} />
-                </Col>
-                <Col md="4" sm="4" xs="8">
-                    <Links />
-                </Col>
-            </Nav>
-        )
-    }
+  render() {
+    return (
+      <Nav>
+        <Col md="8" sm="8" xs="4">
+          <Tabs history={this.props.history} routes={routes} />
+        </Col>
+        <Col md="4" sm="4" xs="8">
+          <Links links={data.links} />
+        </Col>
+      </Nav>
+    );
+  }
 }
 
 export default withRouter(NavBar);
--- src/utils/data.jsx
+++ src/utils/data.jsx
@@ -4,115 +4,135 @@ import gaia from "assets/images/gaia_logo.png";
 import ipu from "assets/images/ipu_logo.png";
 
 const data = {
-    name: "Anant Kaushik",
-    job: "Masters Student",
-    dob: "01 August 1996",
-    email: "anantk@cs.cmu.edu",
-    skype: "live:anantkaushik.me",
+  name: "Anant Kaushik",
+  job: "Masters Student",
+  dob: "01 August 1996",
+  email: "anantk@cs.cmu.edu",
+  skype: "live:anantkaushik.me",
 
-    skills: {
-        top: [
-            {
-                title: "Web Development",
-                desc: "Developed scalable web applications with the latest frameworks and technologies",
-                icon: "flaticon-loading"
-            },
-            {
-                title: "Mobile  Development ",
-                desc: "Experienced in Android development using Java. Created multiple applications and SDK's",
-                icon: "flaticon-responsive"
-            }
-        ],
-        bottom: [
-            {
-                title: "System Architecture",
-                desc: "Designing end-to-end scalable architecture. Currently studying scable systems and architecture",
-                icon: "flaticon-planning"
-            },
-            {
-                title: "Team Management",
-                desc: "Experienced in managing team using agile processes like scrum",
-                icon: "flaticon-brainstorming"
-            }
-        ]
-    },
-
-    workexp: [
-        {
-            photoLink: {
-                src: cmu,
-                alt: "Carnegie Mellon University",
-                link: "https://www.cmu.edu/"
-            },
-            title: "Graduate Teaching Assitant",
-            duration: "Jan 2020 - Present",
-            summary: "Working with Babs Carryer in Entrepreneurship and Innovation in Technology (14815). Mentored students on their “Apprentice Project” and “My Startup” projects."
-        },
-        {
-            photoLink: {
-                src: zup,
-                alt: "Zerone Microsystems Private Limited",
-                link: "http://www.zup.cash"
-            },
-            title: "Lead Developer",
-            duration: "Nov 2018 - May 2019",
-            summary: "Led the development of the initial version of ZUP payments. Created the swipe2.0 protcol for faster transfer of information within smartphone (Android and iOS)."
-        },
-        {
-            photoLink: {
-                src: zup,
-                alt: "Zerone Microsystems Private Limited",
-                link: "http://www.zup.cash"
-            },
-            title: "Senior Developer",
-            duration: "Jul - Oct 2018",
-            summary: "Scaled the Visitor Management System and Guest management Systems for 50% shorter query time. Added analytics dashboard to judge the trends and frequency of guests. Showcased our offerings in Nepal"
-        },
-        {
-            photoLink: {
-                src: zup,
-                alt: "Zerone Microsystems Private Limited",
-                link: "http://www.zup.cash"
-            },
-            title: "Software Engineer",
-            duration: "Jun - Jul 2017",
-            summary: "Developed multiple prototypes from the base technology on Android Platform. Created Visitor Management and Guest Management web portals and mobile application. "
-        },
-        {
-            photoLink: {
-                src: gaia,
-                alt: "Gaia Smart Cities",
-                link: "https://www.gaia.in/index.html"
-            },
-            title: "Software Engineer Intern",
-            duration: "Jun - Jul 2016",
-            summary: "Developed an Android application for a prototype smart scale. Mentored fellow developers on Android applications"
-        }
+  skills: {
+    top: [
+      {
+        title: "Web Development",
+        desc:
+          "Developed scalable web applications with the latest frameworks and technologies",
+        icon: "flaticon-loading",
+      },
+      {
+        title: "Mobile  Development ",
+        desc:
+          "Experienced in Android development using Java. Created multiple applications and SDK's",
+        icon: "flaticon-responsive",
+      },
     ],
+    bottom: [
+      {
+        title: "System Architecture",
+        desc:
+          "Designing end-to-end scalable architecture. Currently studying scable systems and architecture",
+        icon: "flaticon-planning",
+      },
+      {
+        title: "Team Management",
+        desc: "Experienced in managing team using agile processes like scrum",
+        icon: "flaticon-brainstorming",
+      },
+    ],
+  },
 
-    eduexp: [
-        {
-            photoLink: {
-                src: cmu,
-                alt: "Carnegie Mellon University",
-                link: "https://www.cmu.edu/"
-            },
-            title: "Carnegie Mellon University | Masters",
-            duration: "May 2019 - Present",
-            summary: "Pursuing Masters in Software Engineering in School of Computer Science."
-        },
-        {
-            photoLink: {
-                src: ipu,
-                alt: "Guru Gobind Singh Indraprastha University ",
-                link: "http://www.ipu.ac.in/"
-            },
-            title: "GGSIPU | BTech",
-            duration: "Aug 2013 - Jul 2017",
-            summary: "Graduated in first division in Computer Science and Engineering"
-        }
-    ]
+  workexp: [
+    {
+      photoLink: {
+        src: cmu,
+        alt: "Carnegie Mellon University",
+        link: "https://www.cmu.edu/",
+      },
+      title: "Graduate Teaching Assitant",
+      duration: "Jan 2020 - Present",
+      summary:
+        "Working with Babs Carryer in Entrepreneurship and Innovation in Technology (14815). Mentored students on their “Apprentice Project” and “My Startup” projects.",
+    },
+    {
+      photoLink: {
+        src: zup,
+        alt: "Zerone Microsystems Private Limited",
+        link: "http://www.zup.cash",
+      },
+      title: "Lead Developer",
+      duration: "Nov 2018 - May 2019",
+      summary:
+        "Led the development of the initial version of ZUP payments. Created the swipe2.0 protcol for faster transfer of information within smartphone (Android and iOS).",
+    },
+    {
+      photoLink: {
+        src: zup,
+        alt: "Zerone Microsystems Private Limited",
+        link: "http://www.zup.cash",
+      },
+      title: "Senior Developer",
+      duration: "Jul - Oct 2018",
+      summary:
+        "Scaled the Visitor Management System and Guest management Systems for 50% shorter query time. Added analytics dashboard to judge the trends and frequency of guests. Showcased our offerings in Nepal",
+    },
+    {
+      photoLink: {
+        src: zup,
+        alt: "Zerone Microsystems Private Limited",
+        link: "http://www.zup.cash",
+      },
+      title: "Software Engineer",
+      duration: "Jun - Jul 2017",
+      summary:
+        "Developed multiple prototypes from the base technology on Android Platform. Created Visitor Management and Guest Management web portals and mobile application. ",
+    },
+    {
+      photoLink: {
+        src: gaia,
+        alt: "Gaia Smart Cities",
+        link: "https://www.gaia.in/index.html",
+      },
+      title: "Software Engineer Intern",
+      duration: "Jun - Jul 2016",
+      summary:
+        "Developed an Android application for a prototype smart scale. Mentored fellow developers on Android applications",
+    },
+  ],
 
-}
+  eduexp: [
+    {
+      photoLink: {
+        src: cmu,
+        alt: "Carnegie Mellon University",
+        link: "https://www.cmu.edu/",
+      },
+      title: "Carnegie Mellon University | Masters",
+      duration: "May 2019 - Present",
+      summary:
+        "Pursuing Masters in Software Engineering in School of Computer Science.",
+    },
+    {
+      photoLink: {
+        src: ipu,
+        alt: "Guru Gobind Singh Indraprastha University ",
+        link: "http://www.ipu.ac.in/",
+      },
+      title: "GGSIPU | BTech",
+      duration: "Aug 2013 - Jul 2017",
+      summary:
+        "Graduated in first division in Computer Science and Engineering",
+    },
+  ],
+
+  links: {
+    socials: {
+      linkedin: {
+        href: "https://www.linkedin.com/in/anantkas/",
+      },
+      github: {
+        href: "https://github.com/anant-kaushik",
+      },
+    },
+  },
+};
 
 export default data;
